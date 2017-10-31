fabe686ce66510e80b05ba9d5681fad0c2892c6f
George <georgej1088@gmail.com>
2017-10-31 14:12:02 -0400

    Normalize spaces for indentation

--- d20.js
+++ d20.js
@@ -11,18 +11,18 @@
 
 var d20 = {
 
-	/**
-	 * Roll a number of dice and return the result.
-	 *
-	 * @param dice Type of dice to roll, can be represented in various formats:
-	 *               - a number (6, 12, 42)
-	 *               - dice syntax (d20, 4d6, 2d8+2)
-	 * @param verbose Whether or not all dice rolls should be returned as an array
-	 * @return Number|Array
-	 */
-	roll: function(dice, verbose) {
-		var result = d20.verboseRoll(dice),
-			num = 0;
+    /**
+     * Roll a number of dice and return the result.
+     *
+     * @param dice Type of dice to roll, can be represented in various formats:
+     *               - a number (6, 12, 42)
+     *               - dice syntax (d20, 4d6, 2d8+2)
+     * @param verbose Whether or not all dice rolls should be returned as an array
+     * @return Number|Array
+     */
+    roll: function(dice, verbose) {
+        var result = d20.verboseRoll(dice),
+            num = 0;
 
         if(verbose) {
             return result;
@@ -33,79 +33,79 @@ var d20 = {
 
             return num;
         }
-	},
+    },
 
-	/**
-	 * Roll a number of dice and return the result as an array.
-	 *
-	 * @param dice Type of dice to roll, can be represented in various formats:
-	 *               - a number (6, 12, 42)
-	 *               - dice syntax (d20, 4d6, 2d8+2)
-	 * @return Array
-	 */
-	verboseRoll: function(dice) {
-		var amount = 1,
-			mod = 0,
-			results = [],
-			match,
-			num,
-			modifiers;
+    /**
+     * Roll a number of dice and return the result as an array.
+     *
+     * @param dice Type of dice to roll, can be represented in various formats:
+     *               - a number (6, 12, 42)
+     *               - dice syntax (d20, 4d6, 2d8+2)
+     * @return Array
+     */
+    verboseRoll: function(dice) {
+        var amount = 1,
+            mod = 0,
+            results = [],
+            match,
+            num,
+            modifiers;
 
-		if (!dice) {
-			throw new Error('Missing dice parameter.');
-		}
+        if (!dice) {
+            throw new Error('Missing dice parameter.');
+        }
 
-		if (typeof dice == 'string') {
-			match = dice.match(/^\s*(\d+)?\s*d\s*(\d+)\s*(.*?)\s*$/);
-			if (match) {
-				if (match[1]) {
-					amount = parseInt(match[1]);
-				}
-				if (match[2]) {
-					dice = parseInt(match[2]);
-				}
-				if (match[3]) {
-					modifiers = match[3].match(/([+-]\s*\d+)/g);
-					for (var i = 0; i < modifiers.length; i++) {
-						mod += parseInt(modifiers[i].replace(/\s/g, ''));
-					}
-				}
-			} else {
-				parseInt(dice);
-			}
-		}
+        if (typeof dice == 'string') {
+            match = dice.match(/^\s*(\d+)?\s*d\s*(\d+)\s*(.*?)\s*$/);
+            if (match) {
+                if (match[1]) {
+                    amount = parseInt(match[1]);
+                }
+                if (match[2]) {
+                    dice = parseInt(match[2]);
+                }
+                if (match[3]) {
+                    modifiers = match[3].match(/([+-]\s*\d+)/g);
+                    for (var i = 0; i < modifiers.length; i++) {
+                        mod += parseInt(modifiers[i].replace(/\s/g, ''));
+                    }
+                }
+            } else {
+                parseInt(dice);
+            }
+        }
 
-		if (isNaN(dice)) {
-			return [];
-		}
+        if (isNaN(dice)) {
+            return [];
+        }
 
-		for (var i = 0; i < amount; i++) {
-			/* We dont want to ruin verbose, so we dont skip the for loop */
-			if(dice !== 0){
-				num = Math.floor(Math.random() * dice + 1);
-			}else{
-				num = 0;
-			}
-			results.push(num);
-		}
+        for (var i = 0; i < amount; i++) {
+            /* We dont want to ruin verbose, so we dont skip the for loop */
+            if(dice !== 0){
+                num = Math.floor(Math.random() * dice + 1);
+            }else{
+                num = 0;
+            }
+            results.push(num);
+        }
 
-		results = results.sort(function(a, b) {
-			return a - b;
-		});
-		if (mod != 0) {
-			results.push(mod);
-		}
+        results = results.sort(function(a, b) {
+            return a - b;
+        });
+        if (mod != 0) {
+            results.push(mod);
+        }
 
-		return results;
-	}
+        return results;
+    }
 };
 
 if (typeof window != 'undefined') {
-	window.d20 = d20;
+    window.d20 = d20;
 } else if (typeof exports != 'undefined') {
-	for (var k in d20) {
-		exports[k] = d20[k];
-	}
+    for (var k in d20) {
+        exports[k] = d20[k];
+    }
 }
 
 })();
